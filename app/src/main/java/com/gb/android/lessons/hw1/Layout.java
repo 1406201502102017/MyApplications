dcb8294757c503e19adc4806b22937123fa38e53
bekas.omsk@mail.ru <bekas.omsk@mail.ru>
2022-04-21 01:05:40 +0600

    Android. Lesson 1

---
+++ app/src/main/java/com/gb/android/lessons/hw1/Layout.java
@@ -0,0 +1,16 @@
+package com.gb.android.lessons.hw1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gb.android.lessons.R;
+
+public class Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
