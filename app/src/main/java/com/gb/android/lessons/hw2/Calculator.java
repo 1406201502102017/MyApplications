d6ecaf85257e68fd981eee01f0af292f2f976c6c
1406201502102017 <92741096+1406201502102017@users.noreply.github.com>
2022-05-09 11:33:44 +0600

    Update Calculator.java

--- app/src/main/java/com/gb/android/lessons/hw2/Calculator.java
+++ app/src/main/java/com/gb/android/lessons/hw2/Calculator.java
@@ -1,45 +1,109 @@
-package com.gb.android.lessons.hw2;
+package com.gb.android.calculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.gb.android.lessons.R;
+public class MainActivity_calculator extends AppCompatActivity {
 
-public class Calculator extends AppCompatActivity {
-
-    Button one, two, three, plus, minus, four, five, six, divide, multiply, seven, eight, nine, zero, point, clean, equals;
+    TextView result;
+    TextView operation;
+    EditText number;
+    Double operand = null;
+    String lastOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calculator);
+        setContentView(R.layout.activity_main_calculator);
 
-        one = (Button) findViewById(R.id.button1);
-        two = (Button) findViewById(R.id.button2);
-        three = (Button) findViewById(R.id.button3);
-        plus = (Button) findViewById(R.id.button4);
-        minus = (Button) findViewById(R.id.button5);
-
-        four = (Button) findViewById(R.id.button6);
-        five = (Button) findViewById(R.id.button7);
-        six = (Button) findViewById(R.id.button8);
-        divide = (Button) findViewById(R.id.button9);
-        multiply = (Button) findViewById(R.id.button10);
-
-        seven = (Button) findViewById(R.id.button11);
-        eight = (Button) findViewById(R.id.button12);
-        nine = (Button) findViewById(R.id.button13);
-        zero = (Button) findViewById(R.id.button20);
-        point = (Button) findViewById(R.id.button22);
-
-        clean = (Button) findViewById(R.id.button21);
-        equals = (Button) findViewById(R.id.button14);
+        result = findViewById(R.id.result);
+        operation = findViewById(R.id.operation);
+        number = findViewById(R.id.number);
     }
 
-    public void clickButton(View view) {
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if (operand != null) {outState.putDouble("OPERAND", operand);}
+        super.onSaveInstanceState(outState);
+    }
 
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand = savedInstanceState.getDouble("OPERAND");
+        result.setText(operand.toString());
+        operation.setText(lastOperation);
+    }
+
+    public void onNumberClick(View view) {
+
+        Button button = (Button) view;
+        number.append(button.getText());
+
+        if (lastOperation.equals("=") && operand != null) {
+            operand = null;
+        }
+    }
+
+    public void onOperationClick(View view) {
+
+        Button button = (Button) view;
+        String op = button.getText().toString();
+        String numberTwo = number.getText().toString();
+
+        if (numberTwo.length() > 0) {
+            numberTwo = numberTwo.replace(',', '.');
+            try {
+                performOperation(Double.valueOf(numberTwo), op);
+            } catch (NumberFormatException e) {
+                number.setText("");
+            }
+        }
+        lastOperation = op;
+        operation.setText(lastOperation);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void performOperation(Double numberTwo, String operation) {
+        if (operand == null) {
+            operand =numberTwo;
+        } else {
+            if (lastOperation.equals("=")) {
+                lastOperation = operation;
+            }
+            switch (lastOperation) {
+                case "=":
+                    operand = numberTwo;
+                    break;
+                case "/":
+                    if (numberTwo == 0) {
+                        operand = 0.0;
+                    } else {
+                        operand /= numberTwo;
+                    }
+                    break;
+                case "*":
+                    operand *= numberTwo;
+                    break;
+                case "+":
+                    operand += numberTwo;
+                    break;
+                case "-":
+                    operand -= numberTwo;
+                    break;
+            }
+        }
+        result.setText(operand.toString().replace('.', ','));
+        number.setText("");
     }
 }
